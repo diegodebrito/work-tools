755ff257e4ebc825282358fc63a9d9b5f66784be
Diego de Brito <diegosiebra13@gmail.com>
2020-08-13 10:28:58 -0400

    add files

---
+++ python/auxiliary.py
@@ -0,0 +1,14 @@
+
+
+
+database = None
+
+def set_database():
+	global database
+	database = 100
+
+
+
+if __name__ == '__main__':
+	for i in range(10):
+		print(i)
---
+++ python/main.py
@@ -0,0 +1,4 @@
+from auxiliary import set_database, database
+
+set_database()
+print(database)
